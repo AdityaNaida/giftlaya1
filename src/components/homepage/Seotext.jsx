d8543223706d50c08126a37f0bc0a6d196629073
Aditya Naida <adityanaida@gmail.com>
2024-01-26 11:35:29 +0530

    first commit

--- src/components/homepage/Seotext.jsx
+++ src/components/homepage/Seotext.jsx
@@ -3,7 +3,7 @@ const Seotext=()=>{
     return(
         <>
         <div className={styles.BannerHeading}>
-        <h1>Celebrate Occasions with <span>India's no. 1 Gifting Website</span> </h1>
+        <h1>Celebrate Occasions with <span>Indias no. 1 Gifting Website</span> </h1>
         <p>Trusted by 5 Million+ Indians.</p>
         </div>
       
